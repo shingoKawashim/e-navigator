df7e8c15c7703626edc326c8d7873c2c11f92325
Shingo Kawashima <shingo.kawashima@one-star.jp>
2018-06-18 15:24:02 +0900

    削除フラグの定数化（マジックナンバー回避）

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
+  ALIVE = 0
+  DELETED = 1
+
   self.abstract_class = true
 
   def soft_delete
-    self.deleted    = 1 if respond_to?(:deleted)
+    self.deleted    = DELETED if respond_to?(:deleted)
     self.deleted_at = Time.now if respond_to?(:deleted_at)
     save(validate: false)
   end
@@ -13,7 +16,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   class << self
     def alive_records
-      where(deleted: 0)
+      where(deleted: ALIVE)
     end
   end
 end
