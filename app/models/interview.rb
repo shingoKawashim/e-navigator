d8d7866bef8eba964399798a538706b52721b7c7
Shingo Kawashima <shingo.kawashima@one-star.jp>
2018-06-26 16:37:30 +0900

    承認された面談情報の表示

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -6,6 +6,14 @@ class Interview < ApplicationRecord
   validates :user_id, presence: true
   validates :start_date, presence: true
 
+  def show_date
+    self.start_date.strftime("%Y年%m月%d日 %H時%M分〜")
+  end
+
+  def mentor_name
+    User.find(self.mentor_id).name
+  end
+
   class << self
     def alive_future_records
       alive_records.where("start_date >= ?", Date.today).order(id: :asc)
