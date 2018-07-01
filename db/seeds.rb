6707d44a56f5aa737417a326d1caf3b00252f383
Shingo Kawashima <shingo.kawashima@one-star.jp>
2018-07-01 16:14:17 +0900

    seedファイルの見栄えを整える

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "川島慎語", birthday: "1990-08-12", sex: 0, school: "ワンスター", email: "shingo.kawashima@one-star.jp", password: "123", user_type: 0)
-User.create(name: "メンター1", birthday: "1990-01-01", sex: 0, school: "メンター1", email: "mentor_1@gmail.com", password: "123", user_type: 1)
-User.create(name: "メンター2", birthday: "1990-01-01", sex: 1, school: "メンター2", email: "mentor_2@gmail.com", password: "123", user_type: 1)
+User.create(
+  name: "川島慎語",
+  birthday: "1990-08-12",
+  sex: 0,
+  school: "ワンスター",
+  email: "shingo.kawashima@one-star.jp",
+  password: "123",
+  user_type: 0
+)
+User.create(
+  name: "メンター1",
+  birthday: "1990-01-01",
+  sex: 0,
+  school: "メンター1",
+  email: "mentor_1@gmail.com",
+  password: "123",
+  user_type: 1
+)
+User.create(
+  name: "メンター2",
+  birthday: "1990-01-01",
+  sex: 1,
+  school: "メンター2",
+  email: "mentor_2@gmail.com",
+  password: "123",
+  user_type: 1
+)
 
 10.times do |number|
-  User.create(name: "テスト#{number}", birthday: "1990-01-01", sex: 0, school: "テスト#{number}", email: "test_#{number}@gmail.com", password: "123", user_type: 0)
+  User.create(
+    name: "テスト#{number}",
+    birthday: "1990-01-01",
+    sex: 0,
+    school: "テスト#{number}",
+    email: "test_#{number}@gmail.com",
+    password: "123",
+    user_type: 0
+  )
 end
 
 20.times do |number|
